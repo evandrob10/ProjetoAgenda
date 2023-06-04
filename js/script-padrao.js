7e91386cdede5f7afd5c7dc270eda1e5dfafe62d
Evandro Barros <evandrobarrosb98@gmail.com>
2023-06-04 20:34:04 -0300

    Incluido submenu, criado script submenu, criado estilo submenu

---
+++ js/script-padrao.js
@@ -0,0 +1,15 @@
+let submenu = document.querySelectorAll(".cabecalho ul li div");
+
+submenu.forEach((Element)=>{
+    Element.addEventListener("click",()=>{
+        let item = Element.querySelector("ol");
+        let item2 = Element.querySelector("img");
+        item.classList.toggle("submenu-oculto");
+        document.addEventListener("click",(e)=>{
+            if(!(item === e.target || item2 === e.target || Element === e.target)){
+                item.classList.add("submenu-oculto");
+            }
+        })
+    }) 
+})
+
