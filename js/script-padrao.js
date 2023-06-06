679c64244c5a05a7d4c69604af57777531729a08
Evandro Barros <evandrobarrosb98@gmail.com>
2023-06-06 19:36:52 -0300

    Update pagina tarefas e aplicacao do estilo

--- js/script-padrao.js
+++ js/script-padrao.js
@@ -4,11 +4,7 @@ submenu.forEach((Element)=>{
         let item = Element.querySelector("ol");
         let item2 = Element.querySelector("img");
         item.classList.toggle("submenu-oculto");
-        document.addEventListener("click",(e)=>{
-            if(!(item === e.target || item2 === e.target || Element === e.target)){
-                item.classList.add("submenu-oculto");
-            }
-        })
+        document.addEventListener("click", e => {if(!(item === e.target || item2 === e.target || Element === e.target)) item.classList.add("submenu-oculto")});
     }) 
 })
 
