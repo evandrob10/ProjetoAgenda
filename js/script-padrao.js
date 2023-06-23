46f03edaf2405402bab9572db1ee644c5710f5dd
Evandro Barros <evandrobarrosb98@gmail.com>
2023-06-23 01:16:05 -0300

    Update de atuailzação automatica do status: atrasado

--- js/script-padrao.js
+++ js/script-padrao.js
@@ -1,10 +1,26 @@
 let submenu = document.querySelectorAll(".cabecalho ul li div");
-submenu.forEach((Element)=>{
-    Element.addEventListener("click",()=>{
-        let item = Element.querySelector("ol");
-        let item2 = Element.querySelector("img");
-        item.classList.toggle("submenu-oculto");
-        document.addEventListener("click", e => {if(!(item === e.target || item2 === e.target || Element === e.target)) item.classList.add("submenu-oculto")});
-    }) 
-})
+setTimeout(()=>{
+    submenu.forEach((Element)=>{
+        Element.addEventListener("click",()=>{
+            let item = Element.querySelector("ol");
+            let item2 = Element.querySelector("img");
+            item.classList.toggle("submenu-oculto");
+            document.addEventListener("click", e => {if(!(item === e.target || item2 === e.target || Element === e.target)) item.classList.add("submenu-oculto")});
+        })
+    })
+},500);
+(function(){
+    let dataAtual = new Date();
+    let diffDats = dataNova => Math.ceil((dataNova - dataAtual) / (1000 * 3600 * 24));
+    let atrasados = Object.keys(localStorage).filter((atividadeKey)=>{
+        return diffDats(new Date(JSON.parse(localStorage.getItem(atividadeKey)).data)) < 0;
+    });
+    atrasados.filter((atividadeKey)=>{
+        let dadosDoAtrasado =  JSON.parse(localStorage.getItem(atividadeKey));
+        if(dadosDoAtrasado.status !== "concluido"){
+            dadosDoAtrasado.status = "atrasados";
+            localStorage.setItem(atividadeKey,JSON.stringify(dadosDoAtrasado));
+        }
+    })
+})();
 
